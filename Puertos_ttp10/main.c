48670d256451a51ddac36217408a7719affa4f53
maga <maga@debian>
2020-05-19 17:22:57 -0300

    Inicio del programa, vacias los archivos

---
+++ Puertos_ttp10/main.c
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: maga
+ *
+ * Created on May 19, 2020, 5:20 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(void) {
+
+    
+    return 0;
+}
+
