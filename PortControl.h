4868ffa0a8e3de0a704baf7a4b3b628598f051dc
axel <axel@axel-R430-R480>
2020-05-20 12:30:29 -0300

    probando

---
+++ PortControl.h
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   PortControl.h
+ * Author: axel
+ *
+ * Created on 20 de mayo de 2020, 12:29
+ */
+
+#ifndef PORTCONTROL_H
+#define PORTCONTROL_H
+
+
+
+#endif /* PORTCONTROL_H */
+
