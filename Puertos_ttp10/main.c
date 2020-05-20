e4e32901fd988da41b25c748896710acf9647d4e
maga <maga@debian>
2020-05-20 11:50:22 -0300

    Inicio del programa

--- Puertos_ttp10/main.c
+++ Puertos_ttp10/main.c
@@ -13,12 +13,59 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <stdint.h>
 /*
  * 
  */
+
 int main(void) {
 
+    typedef struct 
+    {
+        uint8_t b0   :1;
+        uint8_t b1   :1;
+        uint8_t b2   :1;
+        uint8_t b3   :1;
+        uint8_t b4   :1;
+        uint8_t b5   :1;
+        uint8_t b6   :1;
+        uint8_t b7   :1;
+    } port_t;
+    
+    typedef struct
+    {
+        uint16_t b0  :1;
+        uint16_t b1  :1;
+        uint16_t b2  :1;
+        uint16_t b3  :1;
+        uint16_t b4  :1;
+        uint16_t b5  :1;
+        uint16_t b6  :1;
+        uint16_t b7  :1;
+        uint16_t b8  :1;
+        uint16_t b9  :1;
+        uint16_t b10  :1;
+        uint16_t b11  :1;
+        uint16_t b12  :1;
+        uint16_t b13  :1;
+        uint16_t b14  :1;
+        uint16_t b15  :1;
+        
+    }    
+    
+    
+    typedef union
+    {
+        typedef struct 
+        {
+            port_t port_a;
+            port_t port_b;
+        }port16_t;
+        
+    }
+    
+    
+    
     
     return 0;
 }
