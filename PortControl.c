361426b4f0425cfc6959f3ff0a826d10e37766bd
axel <axel@axel-R430-R480>
2020-05-22 03:05:25 -0300

    probando

--- PortControl.c
+++ PortControl.c
@@ -86,17 +86,39 @@ int bitGet (int bit, char puerto)
 
 void bitToggle (int bit, char puerto)
 {
-    
+    if (((puerto== 'd')||(puerto=='D')) && (bit>=0) && (bit<=15))
+    {
+        uint16_t mask= 0x0001; 
+        mask <<= bit;
+        port.D ^= mask;
+    }
+    else if ((bit>=0) && (bit<=7) && ((puerto=='b')||(puerto=='B')))
+    {
+        uint8_t mask= 0x01;
+        mask<<= bit;
+        port.B ^= mask;
+        
+    }
+    else if((bit>=0) && (bit<=7) && ((puerto=='a')||(puerto=='A')))
+    {
+        uint8_t mask= 0x01;
+        mask<<= bit;
+        port.A ^= mask;
+    }
+    else
+    {
+        printf("Datos mal ingresados\n");
+    }
 }
-void maskOn (int bit, char puerto)
+void maskOn (int mask, char puerto)
 {
     
 }
-void maskOff (int bit, char puerto)
+void maskOff (int mask, char puerto)
 {
     
 }
-void maskToggle (int bit, char puerto)
+void maskToggle (int mask, char puerto)
 {
     
 }
